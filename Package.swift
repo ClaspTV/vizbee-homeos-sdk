5d589eb9f5186835e80bafc29694fb6d34e2fd53
shiva <shiva@bizbee.tv>
2022-06-26 23:06:34 +0530

    Created homeos xcframework

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "VizbeeHomeOSKit",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "VizbeeHomeOSKit",
+            targets: ["VizbeeHomeOSKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "VizbeeHomeOSKit",
+            path: "./VizbeeHomeOSKit.xcframework"
+        ),
+    ]
+)
